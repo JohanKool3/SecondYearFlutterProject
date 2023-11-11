3a8485836d50408a4a8765f60de08e0fb6f7ba46
Ecstatik <johankool2003@hotmail.co.uk>
2023-11-11 13:10:50 +0000

    Added framework for the game state manager

---
+++ lib/Backend/Managers/game_state_manager.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_application_1/Backend/Enums/difficulty.dart';
+import 'package:flutter_application_1/Backend/Low%20Level%20Classes/game_state.dart';
+
+class GameStateManager {
+  late GameState currentState;
+  Difficulty difficulty = Difficulty.easy;
+
+  GameStateManager() {
+    //Try to load first! If you cant load then generate a new one
+    if (!loadGameState()) {
+      newGameState();
+    }
+  }
+
+  void newGameState() {
+    currentState = GameState(difficulty, 0);
+  }
+
+  void setNewDifficulty(Difficulty newDifficulty) => difficulty = newDifficulty;
+
+  bool loadGameState() {
+    // TODO: Load From Database
+
+    return false;
+  }
+}
