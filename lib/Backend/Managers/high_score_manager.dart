77a50c19f54be3cb15701ed84e8b89225cb16c65
Ecstatik <johankool2003@hotmail.co.uk>
2023-11-14 17:41:05 +0000

    Added highscore manager tests and object

--- lib/Backend/Managers/high_score_manager.dart
+++ lib/Backend/Managers/high_score_manager.dart
@@ -1,38 +1,35 @@
 import 'package:flutter_application_1/Backend/Enums/difficulty.dart';
 import 'package:flutter_application_1/Backend/Low%20Level%20Classes/highscore_entry.dart';
 
-class HighscoreManager{
-
-
+class HighscoreManager {
   // Highscores for each difficulty
   late List<HighscoreEntry> hardScores;
   late List<HighscoreEntry> mediumScores;
   late List<HighscoreEntry> easyScores;
 
-  HighscoreManager(){
+  HighscoreManager() {
     hardScores = [];
     mediumScores = [];
     easyScores = [];
   }
 
-  Iterable<HighscoreEntry> getHighscores(Difficulty difficulty, int range){
-
-    if(range < 0){
+  Iterable<HighscoreEntry> getHighscores(Difficulty difficulty, int range) {
+    if (range < 0) {
       throw Exception("Range cannot be negative");
     }
 
-    switch(difficulty){
+    switch (difficulty) {
       case Difficulty.easy:
-        return easyScores.getRange(0, range);
+        return _returnEasyScores(range);
       case Difficulty.medium:
-        return mediumScores.getRange(0, range);
+        return _returnMediumScores(range);
       case Difficulty.hard:
-        return hardScores.getRange(0, range);
+        return _returnHardScores(range);
     }
   }
 
-  void addHighscore(HighscoreEntry entry){
-    switch(entry.difficulty){
+  void addHighscore(HighscoreEntry entry) {
+    switch (entry.difficulty) {
       case Difficulty.easy:
         easyScores.add(entry);
         easyScores.sort((a, b) => a.time.compareTo(b.time));
@@ -48,19 +45,41 @@ class HighscoreManager{
     }
   }
 
-  void saveContents(){
+  void saveContents() {
     // TODO: Integrate with database manager
   }
 
-  void importContents(){
+  void importContents() {
     // TODO: Integrate with database manager
   }
 
-  void importTestData(List<HighscoreEntry> testData){
-
-    for(HighscoreEntry entry in testData){
+  void importTestData(List<HighscoreEntry> testData) {
+    for (HighscoreEntry entry in testData) {
       addHighscore(entry);
     }
   }
 
+  Iterable<HighscoreEntry> _returnEasyScores(int range) {
+    if (range > easyScores.length) {
+      throw Exception("Range cannot be greater than the number of scores");
+    } else {
+      return easyScores.getRange(0, range);
+    }
+  }
+
+  Iterable<HighscoreEntry> _returnMediumScores(int range) {
+    if (range > mediumScores.length) {
+      throw Exception("Range cannot be greater than the number of scores");
+    } else {
+      return mediumScores.getRange(0, range);
+    }
+  }
+
+  Iterable<HighscoreEntry> _returnHardScores(int range) {
+    if (range > hardScores.length) {
+      throw Exception("Range cannot be greater than the number of scores");
+    } else {
+      return hardScores.getRange(0, range);
+    }
+  }
 }
--- tests/Backend Tests/Managers/high_score_manager_tests.dart
+++ tests/Backend Tests/Managers/high_score_manager_tests.dart
@@ -5,19 +5,19 @@ import 'package:test/test.dart';
 
 void main() {
   group('High Score Manager, fields', () {
-    test('High Score Manager, initial hard scores contents should be empty', () {
+    test('Initial hard scores contents should be empty', () {
       HighscoreManager test = HighscoreManager();
 
       expect(test.hardScores, isEmpty);
     });
 
-    test('High Score Manager, initial medium scores contents should be empty', () {
+    test('Initial medium scores contents should be empty', () {
       HighscoreManager test = HighscoreManager();
 
       expect(test.mediumScores, isEmpty);
     });
 
-    test('High Score Manager, initial easy scores contents should be empty', () {
+    test('Initial easy scores contents should be empty', () {
       HighscoreManager test = HighscoreManager();
 
       expect(test.easyScores, isEmpty);
@@ -25,15 +25,210 @@ void main() {
   });
 
   group('High Score Manager, methods', () {
-    test('High Score Manager, addEntry to hard highscores', () {
+    test('AddEntry to hard highscores', () {
       HighscoreManager test = HighscoreManager();
-      HighscoreEntry entry = HighscoreEntry("Test",Difficulty.hard, 100);
+      HighscoreEntry entry = HighscoreEntry("Test", Difficulty.hard, 100);
 
       test.addHighscore(entry);
 
       expect(test.hardScores, contains(entry));
     });
 
-    // TODO: Implement other method tests here
+    test('GetHighscores works for easy difficulty', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(test.getHighscores(Difficulty.easy, 5).length, 5);
+    });
+
+    test('GetHighscores works for medium difficulty', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(test.getHighscores(Difficulty.medium, 5).length, 5);
+    });
+
+    test('GetHighscores works for hard difficulty', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(test.getHighscores(Difficulty.hard, 5).length, 5);
+    });
+
+    test('Highscores are being sorted correctly, easy', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(test.getHighscores(Difficulty.easy, 5).first, testData[0]);
+    });
+
+    test('Highscores are being sorted correctly, medium', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(test.getHighscores(Difficulty.medium, 5).first, testData[5]);
+    });
+
+    test('Highscores are being sorted correctly, hard', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(test.getHighscores(Difficulty.hard, 5).first, testData[10]);
+    });
+
+    test('Highscores returns error when out of range', () {
+      HighscoreManager test = HighscoreManager();
+
+      List<HighscoreEntry> testData = [
+        HighscoreEntry("player1", Difficulty.easy, 1),
+        HighscoreEntry("player2", Difficulty.easy, 2),
+        HighscoreEntry("player3", Difficulty.easy, 3),
+        HighscoreEntry("player4", Difficulty.easy, 4),
+        HighscoreEntry("player5", Difficulty.easy, 5),
+        HighscoreEntry("player6", Difficulty.medium, 6),
+        HighscoreEntry("player7", Difficulty.medium, 7),
+        HighscoreEntry("player8", Difficulty.medium, 8),
+        HighscoreEntry("player9", Difficulty.medium, 9),
+        HighscoreEntry("player10", Difficulty.medium, 10),
+        HighscoreEntry("player11", Difficulty.hard, 11),
+        HighscoreEntry("player12", Difficulty.hard, 12),
+        HighscoreEntry("player13", Difficulty.hard, 13),
+        HighscoreEntry("player14", Difficulty.hard, 14),
+        HighscoreEntry("player15", Difficulty.hard, 15),
+      ];
+
+      for (HighscoreEntry entry in testData) {
+        test.addHighscore(entry);
+      }
+
+      expect(() => test.getHighscores(Difficulty.easy, 10),
+          throwsA(isA<Exception>()));
+    });
   });
 }
