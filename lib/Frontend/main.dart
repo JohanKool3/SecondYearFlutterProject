dce02cfc964999b57a421d43eabf66c8728b45af
Ecstatik <johankool2003@hotmail.co.uk>
2023-11-29 13:53:22 +0000

    added dependencies and front end skeleton

--- lib/Frontend/main.dart
+++ lib/Frontend/main.dart
@@ -1,15 +1,18 @@
 import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Backend/Database/Models/HighScore/GameState/game_state_model.dart';
 import 'package:flutter_application_1/Backend/minesweeper_backend.dart';
 import 'package:flutter_application_1/Frontend/minesweeper.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:flutter_application_1/Backend/Enums/input_type.dart';
 
 late MinesweeperBackend backend;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
 
   await Hive.initFlutter();
   Hive.registerAdapter(GameStateModelAdapter());
@@ -17,11 +20,13 @@ void main() async {
   backend = MinesweeperBackend(
       await Hive.openBox<GameStateModel>('gameStateProduction'));
 
-  // runApp(
-  //   const MaterialApp(
-  //     home: MyFlutterApp(),
-  //   ),
-  // );
+  Minesweeper game = Minesweeper(backend);
+
+  // When in debugging mode, removes the need to keep reloading
+  runApp(
+    GameWidget(game: kDebugMode ? Minesweeper(backend) : game),
+  );
+
   runApp(GameWidget(game: Minesweeper(backend)));
   backend.saveGameState();
   Hive.close();
--- lib/Frontend/minesweeper.dart
+++ lib/Frontend/minesweeper.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Backend/minesweeper_backend.dart';
 
 class Minesweeper extends FlameGame {
-
   late MinesweeperBackend backend;
   Minesweeper(this.backend);
+
+  @override
+  Future<void> onLoad() async {
+    // Create an instance of the Components of the game
+
+    addAll([]);
+  }
 
   @override
   void update(double dt) {
